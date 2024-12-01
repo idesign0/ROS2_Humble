8f41bc6cb5ab93ae539e67ac8e996431e5ba0257
Dhruv Patel <idesign0>
2024-12-01 08:59:15 +0100

    project turtlesim_catch_them_all

--- src/turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_spawner.py
+++ src/turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_spawner.py
@@ -3,41 +3,55 @@ import rclpy
 from rclpy.node import Node
 from random import uniform
 from turtlesim.srv import Spawn
-from turtlesim.srv import Kill 
+from turtlesim.srv import Kill
 from my_robot_interfaces.msg import Turtle
-from my_robot_interfaces.msg import TurtleArray
+
 from functools import partial
  
 class turtle_spawner(Node):
     def __init__(self):
         super().__init__("turtle_spawner")
+
+        self.turtle = Turtle()
+        
+        self.pub_turtleList = self.create_publisher(Turtle,"alive_turtles",10)
+        timer_ = self.create_timer(0.5,self.call_spawn_srv)
+
         self.call_spawn_srv()
-        self.aliveTurtles = TurtleArray()
 
     def call_spawn_srv(self):
+
+        x = uniform(0,10)
+        y = uniform(0,10)
+        thta = uniform(0,2)
+
         new_turtle_srv = self.create_client(Spawn,"/spawn")
 
         while(not new_turtle_srv.wait_for_service(1.0)):
             self.get_logger().warn("Waiting for Service: Spawn to start..")
-
+        
         request = Spawn.Request()
-        request.x = uniform(0,11)
-        request.y = uniform(0,11)
-        request.theta =uniform(0,3.14)
+        request.x = x
+        request.y = y
+        request.theta = thta
     
         future = new_turtle_srv.call_async(request)
-        
-        future.add_done_callback(partial(self.callback_add_turtle,request))
+        future.add_done_callback(partial(self.callback_add_turtles,request = request))
 
-    def callback_add_turtle(self,future,request):
+    def callback_add_turtles(self,future,request):
         try:
-            response = future.result()
-            request.name = response.name
+            request.name = future.result().name
+            turtle = Turtle()
+            turtle.x = request.x
+            turtle.y = request.y
+            turtle.theta = request.theta
+            turtle.name = request.name
 
-            self.aliveTurtle = self.aliveTurtle + request
+            self.pub_turtleList.publish(turtle)
 
         except Exception as e:
             self.get_logger().error("Service call Failed %r" %(e,))
+
 
     def call_kill_srv(self):
         kill_turtle_client = self.create_client(Kill,"/Kill")
@@ -46,6 +60,7 @@ class turtle_spawner(Node):
 def main(args=None):
     rclpy.init(args=args)
     node = turtle_spawner()
+    rclpy.spin(node)
     rclpy.shutdown()
  
  
