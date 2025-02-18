3825055d110d086c975e77bc194ae528f1ccf481
Dhruv Patel <idesign0>
2025-02-18 19:13:36 +0100

    maintaining

--- build/turtlesim_catch_them_all_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/parallels/ros2_ws/src/turtlesim_catch_them_all_bringup
-# Build directory: /home/parallels/ros2_ws/build/turtlesim_catch_them_all_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
