bf03cba125ec3e9b71396571ffcbf394605d8b72
Dhruv Patel <idesign0>
2025-02-18 20:25:27 +0100

    ..

---
+++ build/my_cpp_pkg/CMakeFiles/hardwarestatus_sub.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hardwarestatus_sub.dir/src/hardwarestatus_sub.cpp.o"
+  "CMakeFiles/hardwarestatus_sub.dir/src/hardwarestatus_sub.cpp.o.d"
+  "hardwarestatus_sub"
+  "hardwarestatus_sub.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hardwarestatus_sub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
