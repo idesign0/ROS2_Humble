f8dc2a1b03b84f054ba8673098e76501dac251f9
Dhruv Patel <idesign0>
2025-04-10 17:09:33 +0200

    feedback and goal_state mechanism in Action server

--- src/actions_py/actions_py/count_until_client.py
+++ src/actions_py/actions_py/count_until_client.py
@@ -2,7 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
-from rclpy.action.client import ClientGoalHandle
+from rclpy.action.client import ClientGoalHandle, GoalStatus
 from my_robot_interfaces.action import CountUntil
  
  
@@ -10,10 +10,11 @@ class CountUntilClient(Node): # count_until_client
     def __init__(self):
         super().__init__("count_until_client") # count_until_client
         self.count_until_client_ = ActionClient(self,CountUntil,"count_until")
-    
+
     def send_goal(self,target_number,period):
         # wait for the server
-        self.count_until_client_.wait_for_server()
+        while not self.count_until_client_.wait_for_server(1.0):
+            self.get_logger().info("Client is waiting for the server to start.")
         
         # goal
         goal = CountUntil.Goal()
@@ -22,18 +23,39 @@ class CountUntilClient(Node): # count_until_client
  
         # sending goal
         self.get_logger().info("Sending Goal")
-        self.count_until_client_.send_goal_async(goal).add_done_callback(self.goal_response_callback)
-        
+        self.count_until_client_.send_goal_async(goal,feedback_callback=self.goal_feedback_callback).add_done_callback(self.goal_response_callback)
+        self.timer_ = self.create_timer(2.0,self.cancel_goal)
+
+    def cancel_goal(self):
+        self.get_logger().info("Send a cancel Request.")
+        self.goal_handle_.cancel_goal_async()
+        self.timer_.cancel()
 
     def goal_response_callback(self,future):
         self.goal_handle_:ClientGoalHandle = future.result()
         if self.goal_handle_.accepted:
+            self.get_logger().info("Goal got accepted.")
             self.goal_handle_.get_result_async().add_done_callback(self.goal_result_callback)
+        else:
+             self.get_logger().warn("Goal got rejected")
+
 
     def goal_result_callback(self,future):
+        status = future.result().status
         result = future.result().result
+        
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().info("Success")
+        elif status == GoalStatus.STATUS_ABORTED:
+            self.get_logger().error("Aborted") 
+        elif status == GoalStatus.STATUS_CANCELED:
+            self.get_logger().error("Canceled") 
+
         self.get_logger().info("Result: " + str(result.reached_number))
 
+    def goal_feedback_callback(self,feedback_msg):
+        number = feedback_msg.feedback.current_number
+        self.get_logger().info("Got Feedback: " + str(number))
 
 def main(args=None):
     rclpy.init(args=args)
--- src/actions_py/actions_py/count_until_server.py
+++ src/actions_py/actions_py/count_until_server.py
@@ -2,35 +2,64 @@
 import rclpy
 import time
 from rclpy.node import Node
-from rclpy.action import ActionServer
+from rclpy.action import ActionServer, GoalResponse, CancelResponse
 from rclpy.action.server import ServerGoalHandle
-from my_robot_interfaces.action import CountUntil 
+from my_robot_interfaces.action import CountUntil
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import ReentrantCallbackGroup
  
 class CountUntilServer(Node): # count_until_server
     def __init__(self):
         super().__init__("count_until_server") # MODIFY NAME
         self.count_until_number_server_ = ActionServer(self,CountUntil,"count_until",
-                                                       execute_callback=self.execute_callback)
+                                                       goal_callback=self.goal_callback,
+                                                       cancel_callback=self.goal_cancel_callback,
+                                                       execute_callback=self.execute_callback,
+                                                       callback_group=ReentrantCallbackGroup())
         self.get_logger().info("Action server has been started")
 
+    def goal_callback(self,goal_request:CountUntil.Goal):
+        self.get_logger().info("Received a goal")
+        # Validate the goal request
+        if goal_request.target_number <= 0:
+            self.get_logger().info("Rejecting the goal")
+            return GoalResponse.REJECT
+        self.get_logger().info("Accepting the goal")
+        
+        return GoalResponse.ACCEPT
+
+    def goal_cancel_callback(self,goal_handle:ServerGoalHandle):
+        self.get_logger().info("Recieved a cancel request")
+        return CancelResponse.ACCEPT
+
     def execute_callback(self,goal_handle:ServerGoalHandle):
-        #get the request from the goal
+        # Get the request from the goal
         target_number = goal_handle.request.target_number
         period = goal_handle.request.period
 
         # Execute the action
         self.get_logger().info("Executing the Goal") 
+        feedback = CountUntil.Feedback()
+        result= CountUntil.Result()
         counter = 0
         for i in range(target_number):
+            
+            if goal_handle.is_cancel_requested:
+                self.get_logger().info("Canceling  the goal")
+                goal_handle.canceled()
+                result.reached_number = counter
+                return result
+            
             counter += 1
             self.get_logger().info(str(counter))
+            feedback.current_number = counter
+            goal_handle.publish_feedback(feedback)
             time.sleep(period)
         
         # Once done, set goal Final state
         goal_handle.succeed()
 
         # send the result
-        result= CountUntil.Result()
         result.reached_number = counter
         return result
 
@@ -38,7 +67,7 @@ class CountUntilServer(Node): # count_until_server
 def main(args=None):
     rclpy.init(args=args)
     node = CountUntilServer() # count_until_server
-    rclpy.spin(node)
+    rclpy.spin(node,MultiThreadedExecutor())
     rclpy.shutdown()
  
  
