3cb87c268762f7f8edcd87e5de2ab0a88bc47bcb
Dhruv Patel <dhruvpatel2991998@gmail.com>
2025-04-23 15:16:19 +0200

    Executor: Multi Threaded

--- src/executors_py/executors_py/multi_threaded_executor.py
+++ src/executors_py/executors_py/multi_threaded_executor.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 import time
 
 
 class Node1(Node):
     def __init__(self):
         super().__init__("node1")
-        self.timer1_ = self.create_timer(1.0, self.callback_timer1)
-        self.timer2_ = self.create_timer(1.0, self.callback_timer2)
-        self.timer3_ = self.create_timer(1.0, self.callback_timer3)
+        self.cb_group_1 = MutuallyExclusiveCallbackGroup() # cannot reenter themselves
+        self.cb_group_2 = MutuallyExclusiveCallbackGroup()
+        self.cb_group_3 = MutuallyExclusiveCallbackGroup()
+
+        self.timer1_ = self.create_timer(1.0, self.callback_timer1,callback_group=self.cb_group_1)
+        self.timer2_ = self.create_timer(1.0, self.callback_timer2,callback_group=self.cb_group_2)
+        self.timer3_ = self.create_timer(1.0, self.callback_timer3,callback_group=self.cb_group_3)
 
     def callback_timer1(self):
         time.sleep(2.0)
@@ -27,7 +34,9 @@ class Node1(Node):
 def main(args=None):
     rclpy.init(args=args)
     node1 = Node1()
-    rclpy.spin(node1)
+    executor = MultiThreadedExecutor()
+    executor.add_node(node1)
+    executor.spin()
     rclpy.shutdown()
 
 
