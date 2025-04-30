1598578224f8d4e6b37e12393020b98fa14382d4
Dhruv Patel <dhruvpatel2991998@gmail.com>
2025-04-30 16:21:18 +0200

    FInal Project Base code/node: Spawn and Remove turtle

---
+++ build/turtlesim_final/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/parallels/ros2_ws/ROS2_Humble/src/turtlesim_final
+# Build directory: /home/parallels/ros2_ws/ROS2_Humble/build/turtlesim_final
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
